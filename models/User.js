eb7d5cc788d80c58e935cbfd41f6cdc5989cb876
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-05-27 21:14:04 -0400

    first commit

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Model, Datatypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+//define table columns and configeration
+User.init(
+    {
+        // Table COLUMN DEFINITIONS GO HERE
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [8],
+              // TO DO! how to check the password lenght?
+            },
+
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
