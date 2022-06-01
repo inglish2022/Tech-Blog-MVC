24c62991ec23437b8e44dcdd17c158e5aaaadcc6
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-05-31 20:48:56 -0400

    update Post

--- models/index.js
+++ models/index.js
@@ -2,4 +2,13 @@ const User = require('./User');
 
 const Post = require('./Post');
 
+//create Associations
+Post.belongsTo(User,  {
+    foreignKey:  'user_id',
+});
+
+User.hasMany(Post,  {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
