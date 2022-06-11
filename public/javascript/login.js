f5cf74f38d70d57c9be07754ff723193eae6a3ea
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-06-11 13:30:31 -0400

    feature/handlebar

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,11 +1,40 @@
-const loginFormHandler = function(event) {
+// const loginFormHandler = function(event) {
+//     event.preventDefault();
+//     console.log('working')
+//     const username = document.querySelector('#username-login').value.trim();
+    
+//     const password = document.querySelector('#password-login').value.trim();
+
+//         if (username && password)  {
+//             const response = await fetch('/api/user/login',  {
+//             method: 'POST',
+//             body: JSON.stringify({
+//                 username,
+//                 password
+//             }),
+//             headers: { 'Content-Type': 'application/json' }
+//         })
+//         .then(function() {
+//             document.location.replace('/dashboard');
+//     })
+//     .catch(err => console.log(err));
+
+//     if (response.ok)  {
+//         document.location.replace('/dashboard');
+//     } else {
+//         alert(response.statusText)
+//     }
+//     .catch(err => console.log(err));
+
+// };
+
+async function loginFormHandler(event) {
     event.preventDefault();
     console.log('working')
     const username = document.querySelector('#username-login').value.trim();
-    
     const password = document.querySelector('#password-login').value.trim();
-
-    fetch('/api/user/login', {
+    if (username && password) {
+        const response = await fetch('/api/user/login', {
             method: 'POST',
             body: JSON.stringify({
                 username,
@@ -13,18 +42,17 @@ const loginFormHandler = function(event) {
             }),
             headers: { 'Content-Type': 'application/json' }
         })
-        .then(function() {
-            document.location.replace('/dashboard');
-    })
-    .catch(err => console.log(err));
-
-    // if (response.ok)  {
-    //     document.location.replace('/dashboard');
-    // } else {
-    //     alert(response.statusText)
-    // }
+    //     .then(function() {
+    //         document.location.replace(‘/dashboard’);
+    // })
     // .catch(err => console.log(err));
-
-};
+    if (response.ok)  {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText)
+    }
+    // .catch(err => console.log(err));
+    }
+ };
 
 document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
