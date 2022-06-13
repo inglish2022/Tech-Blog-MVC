c2d00247cb96aee63e2dc66288f9c229c130e39a
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-06-13 19:45:16 -0400

    fixed edit and delete

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,10 +1,12 @@
 async function editPostHandler(event) {
     event.preventDefault();
-    const title = document.getElementById('post-title');
-    const content = document.getElementById('post-content');
-    const post_id = document.getElementById('post_id')
+    const title = document.getElementById('post-title').value;
+    const content = document.getElementById('post-content').value;
+    const post_id = document.getElementById('post-id').value
 
-    const response = await fetch("/api/post/" + post_id.value, {
+    console.log(title, content, post_id)
+
+    const response = await fetch("/api/post/" + post_id, {
         method: "PUT", 
         body: JSON.stringify({
             title,
